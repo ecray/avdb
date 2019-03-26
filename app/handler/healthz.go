e42d31af9edc615d00dbb1eba14dcd8af2a68709
Eric Raymond <eraymond@marqeta.com>
2019-03-26 12:56:02 -0700

    oops, forgot to return

--- app/handler/healthz.go
+++ app/handler/healthz.go
@@ -9,6 +9,7 @@ import (
 func GetHealth(db *gorm.DB, w http.ResponseWriter, r *http.Request) {
 	if _, err := db.Raw("SELECT 1;").Rows(); err != nil {
 		http.Error(w, "ERROR: Database connection not available", http.StatusServiceUnavailable)
+		return
 	}
 	w.Write([]byte("OK"))
 }
